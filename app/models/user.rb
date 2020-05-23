967dfd554dec729aef3356d703b9482cb3ef5414
工藤祐 <kudouyuu@kudouyuunoMacBook-Pro.local>
2020-05-23 14:58:24 +0900

    userモデルにnameカラム追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name,  presence: true,
+                    length: { maximum: 30 }
+
   def self.guest
-    find_or_create_by!(email: 'guest@example.com') do |user|
+    find_or_create_by!(name: 'ゲストユーザー', email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
       # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
     end
