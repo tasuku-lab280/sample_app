788aec0589bd60194d9512916a35eb440425278c
工藤祐 <kudouyuu@kudouyuunoMacBook-Pro.local>
2020-10-18 17:01:23 +0900

    コメントajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,27 +2,28 @@ class CommentsController < ApplicationController
   # 定数
   PERMITTED_ATTRIBUTES = %i(user_id item_id comment)
 
+
+  # フック
+  before_action :authenticate_user!, only: [:create, :destroy]
+
+
   # メソッド
   def create
     @comment = current_user.comments.build(comment_params)
     item = @comment.item
 
     respond_to do |format|
-      if @comment.save!
-        format.html { redirect_to item_path(item) }
+      if @comment.save
+        format.html { redirect_to item_path(item) } 
         format.js
       else
-        format.html { redirect_to item_path(item) }
-        format.js { render :errors }
+        format.html { redirect_to item_path(item) } 
+        format.js
       end
     end
   end
 
   def destroy
-    @comment = Comment.find(params[:id])
-    @comment.destroy!
-    item = @comment.item
-    redirect_to item_path(item)
   end
 
   private
