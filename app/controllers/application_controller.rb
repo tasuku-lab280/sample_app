75267b7298e447a5f9590f2dd42d0c32e53440f5
工藤祐 <kudouyuu@kudouyuunoMacBook-Pro.local>
2020-10-24 17:11:11 +0900

    ヘッダーにお知らせ表示、ログアウト不具合解消

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_notices, if: :user_signed_in?
 
   def check_guest
     email = resource&.email || params[:user][:email].downcase
@@ -14,5 +15,11 @@ class ApplicationController < ActionController::Base
     added_attrs = [ :name, :email, :password, :password_confirmation　]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
+
+  private
+
+  def set_notices
+    @notices = current_user.notices.includes(:sender).order(id: :desc).limit(5)
   end
 end
